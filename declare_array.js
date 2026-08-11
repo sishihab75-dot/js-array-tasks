5e13bbb90827684359eae5b97510446ca0c7bcad
sishihab75-dot <sishihab75@gmail.com>
2026-08-11 21:50:31 +0600

    complite tha task 2

--- declare_array.js
+++ declare_array.js
@@ -1,4 +1,16 @@
+// 1. declare an array 
+
 const array=[1,2,3,4,5];
 console.log(array[3]);
 array[1] = "jambura";
 console.log(array);
+
+// 2.add and remove elements 
+
+const tour=["boga_lake","tindu","dim pahar"];
+tour.push("thanchi");
+tour.unshift("kaptai","cosx bazar ");
+tour.pop();
+tour.shift();
+tour.splice(2,1);
+console.log(tour);
